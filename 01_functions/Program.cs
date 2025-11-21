07217f058ab86b9b3fc130f16b0f52cd2fd5f980
rwijs <rwijs@gmail.com>
2025-11-21 14:48:54 +0100

    03 commit

--- 01_functions/Program.cs
+++ 01_functions/Program.cs
@@ -11,6 +11,42 @@ class Program
     }
     internal void Run()
     {
-        Console.WriteLine();
+        Console.WriteLine("Dit is nu de start van mijn programma");
+    }
+    internal void Vraag1()
+    {
+        Console.WriteLine("Hoe lang zou jij overleven in een zombie apocalypse?");
+        string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
+    }
+    internal void Vraag2()
+    {
+        Console.WriteLine("Why do round pizzas come in square boxes?");
+        string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
+    }
+    internal void Vraag3()
+    {
+        Console.WriteLine("Has someone caught you dancing in front of the mirror?");
+        string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
+    }
+    internal void Vraag4()
+    {
+        Console.WriteLine("Have you ever tried talking to a animal?");
+        string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
+    }
+    internal void Vraag5()
+    {
+        Console.WriteLine("What would you call a male ladybug?");
+        string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
+    }
+    internal void Vraag6()
+    {
+        Console.WriteLine("Which species would be the rudest if all animals could talk?");
+        string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
     }
 }
