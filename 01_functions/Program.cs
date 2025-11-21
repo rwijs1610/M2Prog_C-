b01932116a61b244af498683ee89e31837cc99c3
rwijs <rwijs@gmail.com>
2025-11-21 14:32:04 +0100

    02 commit

--- 01_functions/Program.cs
+++ 01_functions/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        Program program = new Program(); //een program // void // new het maakt een nieuw object van het type Program // hij returned void 
+        program.Run(); 
+    }
+    internal void Run()
+    {
+        Console.WriteLine();
     }
 }
