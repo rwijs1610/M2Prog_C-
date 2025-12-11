52bff43b769af483bf6937ff92efae35092a2ae6
rwijs <rwijs@gmail.com>
2025-12-11 22:07:42 +0100

    04-00 commit

---
+++ 04_lists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace _04_lists;
+
+class Program
+{
+    double[] prijzen = new double[] { 0.99, 5.60, 10.11, 2.50 };
+    string[] artiekelen = new string[] { "snoepje", "luxe broodjes", "lunch menu","koekjes" };
+    Formulier[] formulieren = new Formulier[2];
+
+    internal class Formulier
+    {
+        internal int Sterren { get; set; }
+        internal string Feedback { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Program p = new Program();
+        p.formulieren[0] = new Formulier()
+        {
+            Feedback = "prijzen maar lekker",
+            Sterren = 5
+        };
+        p.formulieren[1] = new Formulier()
+        {
+            Feedback = "eten viel tegen",
+            Sterren = 2
+        };
+        for (int i = 0; i < p.prijzen.Length; i++)
+        {
+            Console.WriteLine(p.artiekelen[i]);
+            Console.WriteLine(p.prijzen[i]);
+        }
+        foreach (Formulier formulier in p.formulieren)
+        {
+            Console.WriteLine($"{formulier.Feedback}");
+            Console.WriteLine($"{formulier.Sterren}");
+        }
+    }
+}
