e40d03c1c0148754505f93c93170e538d8d88a4f
rwijs <rwijs@gmail.com>
2025-12-12 15:19:57 +0100

    04-03 commit

--- 04_lists/2-Lists/Program.cs
+++ 04_lists/2-Lists/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Reflection.Emit;
 using System.Reflection.Metadata.Ecma335;
 using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.Marshalling;
 using System.Runtime.Serialization.Formatters;
 
 namespace _2_Lists;
@@ -30,8 +33,13 @@ class Program
 
     List<string> characterList = new List<string>();
 
-    List<double> reviews = new List<double>(){9.99, 4.68, 342.97, 12.2};
-    
+    List<double> reviews = new List<double>() { 9.99, 4.68, 342.97, 12.2 };
+    List<Pickup> pickups = new List<Pickup>();
+    internal class Pickup
+    {
+        internal int x, y;
+    }
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
@@ -55,11 +63,43 @@ class Program
         // {
         //     Console.WriteLine(item);
         // }
-        reviews.Remove(342.97);
-        reviews.RemoveAt(0);
-        for (int i = 0; i < reviews.Count; i++)
+        // reviews.Remove(342.97);
+        // reviews.RemoveAt(0);
+        // for (int i = 0; i < reviews.Count; i++)
+        // {
+        //     Console.WriteLine(reviews[i]);
+        // }
+        for (int i = 0; i < 10; i++)
         {
-            Console.WriteLine(reviews[i]);
+            Pickup pickup = new Pickup()
+            {
+                x = i,
+                y = 0
+            };
+            pickups.Add(pickup);
         }
+        // foreach (Pickup pickup in pickups)
+        // {
+        //     if(pickup.x == 4)
+        //     {
+        //         pickups.Remove(pickup);
+        //     }
+        // }
+        for (int i = pickups.Count - 1; i >= 0; i--)
+        {
+            if (i == 4)
+            {
+                pickups.RemoveAt(i);
+            }
+        }
+        for (int i = 0; i < pickups.Count; i++)
+        {
+            Console.WriteLine($"Pickup at {pickups[i].x} is still there");
+            if (i == 4)
+            {
+                Console.WriteLine($"Pickup at {i} was removed");
+            }   
+        }
+
     }
 }
