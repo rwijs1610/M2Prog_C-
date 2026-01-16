a572004efee2dfeac123777a0b071e3590cf4893
rwijs <rwijs@gmail.com>
2026-01-16 14:11:58 +0100

    06-1 commit

--- 06-fileio/1-FileIo/Program.cs
+++ 06-fileio/1-FileIo/Program.cs
@@ -4,12 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-Program program = new Program();
+        Program program = new Program();
         program.Run();
     }
 
     void Run()
     {
+        Console.WriteLine(Environment.CurrentDirectory);
 
+    string[] lines = System.IO.File.ReadAllLines("quiz.txt");
+
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+
+    string[] linesNieuws = System.IO.File.ReadAllLines("nieuws.txt");
+
+        foreach (string line in linesNieuws)
+        {
+            Console.WriteLine(line);
+        }
     }
 }
