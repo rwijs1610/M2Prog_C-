d21b805902f5c79cd9ba3acc5b212e344089a432
rwijs <rwijs@gmail.com>
2026-01-16 14:32:31 +0100

    06-2 commit

---
+++ 06-fileio/2-MemSave/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _2_MemSave;
+
+class Program
+{
+    string welkomstTekst = "";
+    string saveFile = "welkomState.txt";
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Run();
+    }
+    internal void Run()
+    {
+        bool bestaatDeFile = File.Exists(saveFile);
+
+        if (bestaatDeFile == true)
+        {
+            welkomstTekst = File.ReadAllText(saveFile);
+        }
+        Console.WriteLine(welkomstTekst);
+
+        while (true)
+        {
+            Console.WriteLine("Enter a text, then press enter");
+            welkomstTekst = Console.ReadLine();
+            if (welkomstTekst.Length > 0)
+            {
+                File.WriteAllText(saveFile, welkomstTekst);
+            }
+        }
+    }
+}
