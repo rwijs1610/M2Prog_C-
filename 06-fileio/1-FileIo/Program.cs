1003410c869098316af9eed2eba3a0687c949485
rwijs <rwijs@gmail.com>
2026-01-16 14:05:56 +0100

    06-1 commit

---
+++ 06-fileio/1-FileIo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _1_FileIo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+Program program = new Program();
+        program.Run();
+    }
+
+    void Run()
+    {
+
+    }
+}
