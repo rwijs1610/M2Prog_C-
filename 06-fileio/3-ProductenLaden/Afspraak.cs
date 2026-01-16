8e2ac371db4d6698f869176e944dec91ffd605a6
rwijs <rwijs@gmail.com>
2026-01-16 15:26:27 +0100

    06-3 commit

---
+++ 06-fileio/3-ProductenLaden/Afspraak.cs
@@ -0,0 +1,10 @@
+
+public class Afspraak
+{
+    public int tijd { get; set; }
+    public string beschrijving { get; set; }
+    public string dag { get; set; }
+}
+
+
+
---
+++ 06-fileio/3-ProductenLaden/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _3_ProductenLaden;
+
+using System.Net.Http.Headers;
+using System.Text.Json;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Run();
+        program.RunProducten();
+        program.RunAfspraak();
+    }
+
+    void Run()
+    {
+        // string text = System.IO.File.ReadAllText("Product.json");
+        // Product product = JsonSerializer.Deserialize<Product>(text);
+        // Console.WriteLine(product.Name);
+        // Console.WriteLine(product.Description);
+        // Console.WriteLine(product.Price);
+
+    }
+    void RunProducten()
+    {
+        string text = System.IO.File.ReadAllText("Producten.json");
+        Product[] product = JsonSerializer.Deserialize<Product[]>(text);
+        foreach (Product p in product)
+        {
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.Description);
+            Console.WriteLine(p.Price);
+        }
+    }
+    void RunAfspraak(){
+        string text = System.IO.File.ReadAllText("Afspraak.json");
+        Afspraak[] afspraak = JsonSerializer.Deserialize<Afspraak[]>(text);
+        foreach (Afspraak a in afspraak)
+        {
+            Console.WriteLine(a.tijd);
+            Console.WriteLine(a.beschrijving);
+            Console.WriteLine(a.dag);
+        }
+    }
+}
